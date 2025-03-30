2cb1865702471f73a74064f39ebccddebce5dda0
Sirjan <sirjanssk2933@protonmail.ch>
2025-03-30 18:53:36 -0400

    Assignmet 9 part 1 complete

--- app/src/main/java/com/example/mad411assignments/File.Kt.kt
+++ app/src/main/java/com/example/mad411assignments/File.Kt.kt
@@ -5,18 +5,18 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 object File {
-    private const val FILE_NAME = "expenses.json"
+    private const val FILE = "expense.json"
 
     fun saveExpense(context: Context, expenses: List<ExpenseRecord>) {
         val json = Gson().toJson(expenses)
-        context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).use {
+        context.openFileOutput(FILE, Context.MODE_PRIVATE).use {
             it.write(json.toByteArray())
         }
     }
 
     fun loadExpense(context: Context): MutableList<ExpenseRecord> {
         return try {
-            val json = context.openFileInput(FILE_NAME).bufferedReader().use { it.readText() }
+            val json = context.openFileInput(FILE).bufferedReader().use { it.readText() }
             val type = object : TypeToken<MutableList<ExpenseRecord>>() {}.type
             Gson().fromJson(json, type)
         } catch (e: Exception) {
