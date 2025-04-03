12dc977c79d3b0e729c60dc43cca55771b61e3fb
Sirjan <sirjanssk2933@protonmail.ch>
2025-04-03 11:22:14 -0400

    Ass 10 changet expense class added currency

--- app/src/main/java/com/example/mad411assignments/ExpenseRecord.kt
+++ app/src/main/java/com/example/mad411assignments/ExpenseRecord.kt
@@ -1,3 +1,9 @@
 package com.example.mad411assignments
 
-data class ExpenseRecord(val title: String, val price: Double, val date: String)
+data class ExpenseRecord(
+    val title: String,
+    val price: Double,
+    val date: String,
+    val currency: String = "CAD",
+    val convertedCost: Double = price
+)
